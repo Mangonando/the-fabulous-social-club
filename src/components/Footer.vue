d70ea748842dc5d2f5bf0fd15c4b85380e7def55
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2022-10-14 00:38:23 +0200

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class='footer'>
-    <img
-      class='logo'
-      src='../assets/logo-the-fabulous-social-club.png'
-      alt='The Fabulous Social Club Logo'
-    />
-    <a href='https://github.com/Mangonando/the-fabulous-social-club'
-      ><img class='logo' src='../assets/github-logo.svg' alt='Github logo'
-    /></a>
-    <a href='https://www.linkedin.com/in/juan-f-ramirez/'
-      ><img class='logo' src='../assets/linkedin-logo.svg' alt='Linkedin logo'
-    /></a>
+  <div class="footer">
+    <div class="icons-container">
+      <a href="https://github.com/Mangonando/the-fabulous-social-club"
+        ><img class="icon" src="../assets/github-logo.svg" alt="Github icon"
+      /></a>
+      <a href="https://www.linkedin.com/in/juan-f-ramirez/"
+        ><img class="icon" src="../assets/linkedin-logo.svg" alt="Linkedin icon"
+      /></a>
+    </div>
+    <div class="logo-container">
+      <img
+        class="logo"
+        src="../assets/logo-the-fabulous-social-club.png"
+        alt="The Fabulous Social Club Logo"
+      />
+    </div>
   </div>
 </template>
 
@@ -24,10 +28,20 @@ export default {
 </script>
 <style scoped>
 .footer {
-  @apply bg-yellow-400 flex justify-center items-center;
+  @apply flex justify-center items-center flex-col;
+}
+
+.icons-container {
+  @apply flex flex-row
+    md:space-x-2 lg:space-x-4;
+}
+
+.icon{
+  @apply w-8 md:w-9 lg:w-12 xl:w-14 h-auto
+    fill-lime-400;
 }
 
 .logo {
-  @apply w-24 md:w-28 lg:w-32 xl:w-48 h-auto;
+  @apply w-20 md:w-36 lg:w-44 xl:w-48 h-auto;
 }
 </style>
